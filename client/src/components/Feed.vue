cbfc4912f9bb686151e5a9e96eb15591e84dcc58
mlande <miclande@gmail.com>
2017-08-24 07:02:55 +0200

    column design

--- client/src/components/Feed.vue
+++ client/src/components/Feed.vue
@@ -19,13 +19,29 @@
     </section>
   
     <div v-if="currentSubject === 'all' || currentSubject === null" class="container">
-      <item v-for="item in sortedItems" :key="item._id" :item="item">
-      </item>
+      <div class="columns">
+        <div class="column">
+          <item v-for="item in slicedItems_1" :itemID="item._id" :key="item._id" :item="item">
+          </item>
+        </div>
+        <div class="column">
+          <item v-for="item in slicedItems_2" :itemID="item._id" :key="item._id" :item="item">
+          </item>
+        </div>
+      </div>
     </div>
   
     <div v-else class="container">
-      <item v-for="item in filteredItems" :key="item._id" :item="item">
-      </item>
+      <div class="columns">
+        <div class="column">
+          <item v-for="item in slicedFilteredItems_1" :itemID="item._id" :key="item._id" :item="item">
+          </item>
+        </div>
+        <div class="column">
+          <item v-for="item in slicedFilteredItems_2" :itemID="item._id" :key="item._id" :item="item">
+          </item>
+        </div>
+      </div>
     </div>
   
   </div>
@@ -64,7 +80,45 @@ export default {
         (first, second) =>
           (second.upvotes.length - first.upvotes.length)
       )
+    },
+    slicedFilteredItems_1() {
+      let len = this.filteredItems.length;
+      let evens = [];
+      for (var i = 0; i < len; i += 2) {
+        evens.push(this.filteredItems[i]);
+      };
+      return evens;
+      //return this.filteredItems.slice(0, this.filteredItems.length / 2)
+    },
+    slicedFilteredItems_2() {
+      let len = this.filteredItems.length;
+      let odds = [];
+      for (var i = 1; i < len; i += 2) {
+        odds.push(this.filteredItems[i]);
+      };
+      return odds;
 
+      //return this.filteredItems.slice(this.filteredItems.length / 2, this.filteredItems.length)
+    },
+    slicedItems_1() {
+      let len = this.sortedItems.length;
+      let evens = [];
+      for (var i = 0; i < len; i += 2) {
+        evens.push(this.sortedItems[i]);
+      };
+      return evens;
+
+      //return this.sortedItems.slice(0, this.sortedItems.length / 2)
+    },
+    slicedItems_2() {
+      let len = this.sortedItems.length;
+      let odds = [];
+      for (var i = 1; i < len; i += 2) {
+        odds.push(this.sortedItems[i]);
+      };
+      return odds;
+
+      //return this.sortedItems.slice(this.sortedItems.length / 2, this.items.length)
     }
   },
   created() {
@@ -82,7 +136,6 @@ export default {
       this.$root.justContributed = false;
     }
   }
-
 }
 </script>
 
--- client/src/components/Item.vue
+++ client/src/components/Item.vue
@@ -1,7 +1,25 @@
 <template>
     <div class="item">
     
-        <div class="box item-box">
+        <div :class="{'is-active': prompt.isActive}" class="modal prompt">
+            <div class="modal-background" @click="togglePrompt"></div>
+            <div class="modal-content">
+                {{ prompt.text }}
+    
+                <div class="notification is-primary">
+                    {{ prompt.text }}
+                    <button class="button is-small">
+                        <span class="icon">
+                            <i class="fa fa-check" @click="deleteItem">
+                            </i>
+                        </span>
+                    </button>
+                </div>
+            </div>
+            <button class="modal-close is-large" aria-label="close"></button>
+        </div>
+    
+        <div class="box item-box" v-if="!removed">
             <article class="media">
                 <div class="media-left">
                     <figure class="image is-64x64">
@@ -11,37 +29,98 @@
                 <div class="media-content">
                     <div class="content">
                         <p>
-                            <strong> {{ item.headline }} </strong>
+    
+                            <strong>
+                                <a :href="item.url"> {{ item.headline }} </a>
+                            </strong>
+                            &nbsp;&nbsp;
+    
                             <small> @{{ username }} </small>
-                            <small> [ score: {{ item.upvotes.length }} ] </small>
+    
                             <br>
                             <small> {{ parsedTimestamp }} </small>
                             <br>
-                            <a :href="item.url"> {{ item.url }} </a>
+                            <a class="link-option" @click="toggleURL"> {{ word }} Full URL </a>
+                            <a :href="item.url" v-if="showFullLink">
+                                <br>{{ item.url }} </a>
+                            <br>
                             <br> {{ item.description }}
                         </p>
                     </div>
+                    <br>
                     <nav class="level is-mobile">
                         <div class="level-left">
                             <a class="level-item">
                                 <span class="icon">
-                                    <i class="fa fa-comment" @click="startComment" :class="{'used': commenting}"></i>
+                                    <i class="fa fa-comment" @click="startComment" :class="{'used': commenting}">
+                                        &nbsp;
+                                    </i>
                                 </span>
                             </a>
                             <a class="level-item">
                                 <span class="icon">
-                                    <i class="fa fa-eye" @click="browse" :class="{'used': browsing}"></i>
+                                    <i class="fa fa-eye" @click="browse" :class="{'used': browsing || !item.comments.length}">
+                                        {{ item.comments.length }}
+                                    </i>
                                 </span>
                             </a>
                             <a class="level-item">
                                 <span class="icon" v-bind:class="{'used': hasVoted}">
-                                    <i class="fa fa-thumbs-up" @click="upvote"></i>
+                                    <i class="fa fa-thumbs-up" @click="upvote">
+                                        {{ item.upvotes.length }}
+                                    </i>
+                                </span>
+                            </a>
+    
+                        </div>
+                        <div class="level-right">
+                            <a class="level-item" @click="togglePrompt" v-if="isOfCurrentUser">
+                                <span class="icon">
+                                    <i class="fa fa-trash"></i>
                                 </span>
                             </a>
                         </div>
                     </nav>
                 </div>
             </article>
+        </div>
+    
+        <div v-else class="columns">
+            <div class="column">
+                <div class="section deleted">
+                    <div class="notification is-primary is-small">
+                        {{ deleted }}
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+            </div>
+        </div>
+    
+        <div class="box" v-if="browsing && item.comments.length">
+            <button class="button is-small is-danger" @click="browse">
+                <span class="icon is-small">
+                    <i class="fa fa-times"></i>
+                </span>
+            </button>
+    
+            <section class="section">
+                <comment v-for="comment in item.comments" :commentID="comment" :key="comment._id" v-on:restart="restartBrowsing(this.commentID)"></comment>
+            </section>
+    
+        </div>
+    
+        <div class="box" v-if="browsing && !item.comments.length">
+            <button class="button is-small is-danger" @click="browse">
+                <span class="icon is-small">
+                    <i class="fa fa-times"></i>
+                </span>
+            </button>
+    
+            <section class="section">
+                (no comments)
+            </section>
+    
         </div>
     
         <div class="box" v-if="commenting">
@@ -69,32 +148,6 @@
             </div>
     
         </div>
-    
-        <div class="box" v-if="browsing && item.comments.length">
-            <button class="button is-small is-success" @click="browse">
-                <span class="icon is-small">
-                    <i class="fa fa-check"></i>
-                </span>
-            </button>
-    
-            <section class="section">
-                <comment v-for="comment in item.comments" :commentID="comment" :key="comment._id" v-on:restart="restartBrowsing(this.commentID)"></comment>
-            </section>
-    
-        </div>
-    
-        <div class="box" v-if="browsing && !item.comments.length">
-            <button class="button is-small is-success" @click="browse">
-                <span class="icon is-small">
-                    <i class="fa fa-check"></i>
-                </span>
-            </button>
-    
-            <section class="section">
-                (no comments)
-            </section>
-    
-        </div>
         <br>
     </div>
 </template>
@@ -107,6 +160,7 @@ export default {
     name: 'item',
     data() {
         return {
+            isOfCurrentUser: false,
             username: null,
             currentComment: {
                 text: "",
@@ -116,7 +170,13 @@ export default {
             browsing: false,
             placeholder: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
             commentDefault: "Share your thoughts, ask a question or answer one :) ",
-            hasVoted: false
+            hasVoted: false,
+            showFullLink: false,
+            prompt: {
+                isActive: false,
+                text: "Remove this item?"
+            },
+            removed: false
         }
     },
     props: [
@@ -127,6 +187,14 @@ export default {
         _getUsername() {
             api.users.getOne(this.item.contributor).then(user => {
                 this.username = user.username
+                this.checkOfUser();
+            })
+        },
+        checkOfUser() {
+            api.users.getOne(this.item.contributor).then(user => {
+                if (user._id === this.$root.user.id) {
+                    this.isOfCurrentUser = true;
+                }
             })
         },
 
@@ -142,16 +210,21 @@ export default {
             if (!this.hasVoted) {
                 this.hasVoted = true;
                 this.item.upvotes.push(window.localStorage.id);
-                api.items.editOne(this.item._id, { upvotes: this.item.upvotes }).catch(err => { alert("problem") })
+                api.items.editOne(this.item._id, { upvotes: this.item.upvotes })
+                    .catch(err => { alert("problem") })
             }
 
         },
 
         startComment() {
-            if (this.browsing) {
+            /* if (this.browsing) {
                 this.browse();
+            } */
+            if (this.commenting) {
+                this.commenting = false;
+            } else {
+                this.commenting = true;
             }
-            this.commenting = true;
         },
 
         postComment() {
@@ -178,9 +251,9 @@ export default {
 
         browse() {
             this.browsing = !this.browsing;
-            if (this.commenting) {
-                this.cancelComment();
-            }
+            /* if (this.commenting) {
+                 this.cancelComment();
+             } */
         },
 
         checkIfVoted() {
@@ -191,6 +264,18 @@ export default {
         restartBrowsing(comment) {
             let list = this.item.comments;
             list.splice(list.indexOf(comment), 1);
+        },
+        toggleURL() {
+            this.showFullLink = !this.showFullLink;
+        },
+        deleteItem() {
+            api.items.deleteOne(this.item._id).then(res => {
+                this.togglePrompt();
+                this.removed = true;
+            });
+        },
+        togglePrompt() {
+            this.prompt.isActive = !this.prompt.isActive
         }
 
 
@@ -198,6 +283,12 @@ export default {
     computed: {
         parsedTimestamp() {
             return this.item.timestamp ? dateformat(new Date(this.item.timestamp), "mmmm dS, HH:MM") : null
+        },
+        word() {
+            return this.showFullLink ? "Hide" : "Show"
+        },
+        deleted() {
+            return "" + this.item.headline + " has been removed";
         }
 
     },
@@ -221,5 +312,23 @@ export default {
 
 .used {
     color: rgba(0, 0, 0, 0.1);
+}
+
+.link-option {
+    color: rgba(0, 0, 0, 0.3);
+    font-size: 0.85em;
+}
+
+prompt {
+    background-color: red;
+}
+
+deleted {
+    width: 100px;
+    color: red;
+}
+
+voted {
+    background-color: red;
 }
 </style>
